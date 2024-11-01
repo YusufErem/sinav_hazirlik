a3abccd9a644f5e0d7d1d0fd870e4145a2b40871
yusuferem <ysferem@gmail.com>
2024-11-01 23:57:35 +0300

    Form sayfalari ve ana sayfa taslaklari olusturuldu ve paketler yeniden duzenlenmek uzere silindi

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import SignUpForm
 
-def signup(request):
+def registerPage(request):
+    form = SignUpForm()
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('/')
-    else:
-        form = SignUpForm()
-    return render(request, 'registration/signup.html', {'form': form})
+            return redirect('home')
+    return render(request, 'register.html', {'form': form})
+
+def loginPage(request):
+    return render(request, 'login.html')
+
